9cdcb450c87a82e4670947f35e1688548e2dc81c
chgyaneswar2004 <chgyaneswar1971@gmail.com>
2025-06-26 21:06:00 +0530

    changes added

--- CodeStation/client/src/App.jsx
+++ CodeStation/client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import Toast from "./components/toast/Toast";
 import EditorPage from "./pages/EditorPage";
 import HomePage from "./pages/HomePage";
