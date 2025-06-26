820aa3ee6a08201323861bc159c12401fc0e239b
varsha-0110 <varshapriyadarshini05@gmail.com>
2025-06-26 15:53:11 +0530

    Your commit message

--- CodeStation/client/src/components/common/Footer.jsx
+++ CodeStation/client/src/components/common/Footer.jsx
@@ -1,22 +1,4 @@
-/*function Footer() {
-    return (
-        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
-            <span>
-                Built with ❤️ by{" "}
-                <a
-                    href="https://github.com/sahilatahar"
-                    className="text-primary underline underline-offset-1"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    sahilatahar
-                </a>
-            </span>
-        </footer>
-    );
-}
 
-export default Footer; */
 function Footer() {
     return (
         <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
